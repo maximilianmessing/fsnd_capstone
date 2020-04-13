ede35bcdfd0ae10c9be9576f285597ed70913509
Maximilian Messing <maximilian.messing@whu.edu>
2020-04-13 11:41:35 +0200

    add RBAC tests and apply small changes to auth configuration

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 from models import setup_db, Movie, Actor
+
+from auth import AuthError, requires_auth
 
 
 def create_app(test_config=None):
@@ -13,7 +13,8 @@ def create_app(test_config=None):
     CORS(app)
 
     @app.route('/movies')
-    def get_movies():
+    @requires_auth('get:movies')
+    def get_movies(token):
         movies = Movie.query.order_by(Movie.id).all()
         if movies == []:
             abort(404)
@@ -24,16 +25,20 @@ def create_app(test_config=None):
         })
 
     @app.route('/movies', methods=["POST"])
-    def create_movie():
+    @requires_auth('post:movies')
+    def create_movie(token):
         body = request.get_json()
 
         new_title = body.get('title', None)
         new_release_date = body.get('release_date', None)
-        new_actors = body.get('actors', None)
+        new_actors = Actor.query.filter(Actor.id.in_(
+            body.get('actors', None))).all()
 
         try:
             movie = Movie(title=new_title,
-                          release_date=new_release_date)
+                          release_date=new_release_date
+                          )
+            movie.actors = new_actors
             movie.insert()
             movies = Movie.query.order_by(Movie.id).all()
             formatted_movies = [movie.format() for movie in movies]
@@ -45,30 +50,32 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/movies/<int:movie_id>', methods=["PATCH"])
-    def edit_movie(movie_id):
+    @requires_auth('patch:movies')
+    def edit_movie(token, movie_id):
         body = request.get_json()
 
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
         if movie is None:
             abort(404)
 
-        # try:
-        movie.title = body.get('title', None)
-        movie.release_date = body.get('release_date', None)
-        movie.actors = Actor.query.filter(Actor.id.in_(
-            body.get('actors', None))).all()
-        movie.update()
-        movies = Movie.query.order_by(Movie.id).all()
-        formatted_movies = [movie.format() for movie in movies]
-        return jsonify({
-            "success": True,
-            "movies": formatted_movies
+        try:
+            movie.title = body.get('title', None)
+            movie.release_date = body.get('release_date', None)
+            movie.actors = Actor.query.filter(Actor.id.in_(
+                body.get('actors', None))).all()
+            movie.update()
+            movies = Movie.query.order_by(Movie.id).all()
+            formatted_movies = [movie.format() for movie in movies]
+            return jsonify({
+                "success": True,
+                "movies": formatted_movies
             })
-        # except Exception:
-        #     abort(422)
+        except Exception:
+            abort(422)
 
     @app.route('/movies/<int:movie_id>', methods=["DELETE"])
-    def delete_movie(movie_id):
+    @requires_auth('delete:movies')
+    def delete_movie(token, movie_id):
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
         if movie is None:
             abort(404)
@@ -84,7 +91,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/actors')
-    def get_actors():
+    @requires_auth('get:actors')
+    def get_actors(token):
         actors = Actor.query.order_by(Actor.id).all()
         if actors == []:
             abort(404)
@@ -95,7 +103,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/actors', methods=["POST"])
-    def create_actor():
+    @requires_auth('post:actors')
+    def create_actor(token):
         body = request.get_json()
 
         new_name = body.get('name', None)
@@ -116,7 +125,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/actors/<int:actor_id>', methods=["PATCH"])
-    def edit_actor(actor_id):
+    @requires_auth('patch:actors')
+    def edit_actor(token, actor_id):
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
         if actor is None:
             abort(404)
@@ -138,7 +148,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/actors/<int:actor_id>', methods=["DELETE"])
-    def delete_actor(actor_id):
+    @requires_auth('delete:actors')
+    def delete_actor(token, actor_id):
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
         if actor is None:
             abort(404)
@@ -192,6 +203,12 @@ def create_app(test_config=None):
             "error": 500,
             "message": "Internal Server error"
         }), 500
+
+    @app.errorhandler(AuthError)
+    def handle_auth_error(ex):
+        response = jsonify(ex.error)
+        response.status_code = ex.status_code
+        return response
 
     return app
 
--- test_app.py
+++ test_app.py
@@ -1,10 +1,45 @@
-import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
 from app import create_app
-from models import setup_db, Movie, Actor
+from models import setup_db
+
+import requests
+
+# testing user password database:
+testingUsers = {
+    'messing.maximilian+executive@gmail.com': 'executive123!',
+    'messing.maximilian+director@gmail.com': 'director123!',
+    'messing.maximilian+assistant@gmail.com': 'assistant123!',
+}
+
+# Taken from https://stackoverflow.com/questions/48552474/
+# auth0-obtain-access-token-for-unit-tests-in-python/48554119#48554119
+
+
+def getUserToken(userName):
+    url = 'https://wcp.eu.auth0.com/oauth/token'
+    headers = {'content-type': 'application/json'}
+    client_secret =\
+        "0L9AdpWZfeXfXAqUOflWqXEWur7fFGPTO_bmxBj4R4M-RN14MISOCOgNvmqA-t80"
+    password = testingUsers[userName]
+    parameter = {"client_id": "UEjRDlMUtQ4GLe5wiKgTRfALDsk35Gee",
+                 "client_secret": client_secret,
+                 "audience": 'casting',
+                 "grant_type": "password",
+                 "username": userName,
+                 "password": password, "scope": "openid"}
+    # do the equivalent of a CURL request from https://auth0.com/docs/
+    # quickstart/
+    # backend/python/02-using#obtaining-an-access-token-for-testing
+    responseDICT = json.loads(requests.post(
+        url, json=parameter, headers=headers).text)
+    return responseDICT['access_token']
+
+
+def getUserTokenHeaders(userName='testingUser2@funnymail.com'):
+    return {'authorization': "Bearer " + getUserToken(userName)}
 
 
 class CastingTestCase(unittest.TestCase):
@@ -35,6 +70,9 @@ class CastingTestCase(unittest.TestCase):
                                   'release_date': "hey there",
                                   'actors': [1],
                                   }
+        self.auth_header_executive = AUTH_HEADER_EXECUTIVE
+        self.auth_header_director = AUTH_HEADER_DIRECTOR
+        self.auth_header_assistant = AUTH_HEADER_ASSISTANT
 
         # binds the app to the current context
         with self.app.app_context():
@@ -47,8 +85,11 @@ class CastingTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
+# All test use executive producer permission as he has all permission.
+# Other RBAC Tests are at the end of the file.
+
     def test_0_get_empty_movie_404(self):
-        res = self.client().get('movies')
+        res = self.client().get('movies', headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -56,7 +97,7 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Resource was not found")
 
     def test_0_get_empty_actor_404(self):
-        res = self.client().get('actors')
+        res = self.client().get('actors', headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -64,7 +105,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Resource was not found")
 
     def test_1_post_actors(self):
-        res = self.client().post('actors', json=self.new_actor)
+        res = self.client().post('actors', json=self.new_actor,
+                                 headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -72,7 +114,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['actors']), 1)
 
     def test_post_invalid_actors_yield_422(self):
-        res = self.client().post('actors', json=self.new_invalid_actor)
+        res = self.client().post('actors', json=self.new_invalid_actor,
+                                 headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -80,7 +123,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Unprocessable Entity")
 
     def test_1_post_movies(self):
-        res = self.client().post('movies', json=self.new_movie)
+        res = self.client().post('movies', json=self.new_movie,
+                                 headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -88,7 +132,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['movies']), 1)
 
     def test_post_invalid_movies_yield_422(self):
-        res = self.client().post('movies', json=self.new_invalid_movie)
+        res = self.client().post('movies', json=self.new_invalid_movie,
+                                 headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -96,7 +141,7 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Unprocessable Entity")
 
     def test_2_get_actors(self):
-        res = self.client().get('actors')
+        res = self.client().get('actors', headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -104,7 +149,7 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['actors']), 1)
 
     def test_2_get_movies(self):
-        res = self.client().get('movies')
+        res = self.client().get('movies', headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -112,7 +157,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['movies']), 1)
 
     def test_patch_non_existing_actor_yield_404(self):
-        res = self.client().patch('actors/5', json=self.new_actor)
+        res = self.client().patch('actors/5', json=self.new_actor,
+                                  headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -120,7 +166,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Resource was not found")
 
     def test_patch_non_existing_movie_yield_404(self):
-        res = self.client().patch('movies/5', json=self.new_movie)
+        res = self.client().patch('movies/5', json=self.new_movie,
+                                  headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -128,10 +175,11 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Resource was not found")
 
     def test_2_patch_actors(self):
-        res = self.client().patch('actors/1', json={'name': 'Maximilian Messing',
-                                                    'age': 25,
-                                                    'gender': 'male',
-                                                    })
+        res = self.client().patch('actors/1',
+                                  json={'name': 'Maximilian Messing',
+                                        'age': 25,
+                                        'gender': 'male',
+                                        }, headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -139,10 +187,12 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['actors']), 1)
 
     def test_2_patch_movies(self):
-        res = self.client().patch('movies/1', json={'title': 'Terminator',
-                                                    'release_date': '2012-12-04',
-                                                    'actors': [1],
-                                                    })
+        res = self.client().patch('movies/1',
+                                  json={'title': 'Terminator',
+                                        'release_date': '2012-12-04',
+                                        'actors': [1],
+                                        },
+                                  headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -150,7 +200,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['movies']), 1)
 
     def test_3_delete_movie(self):
-        res = self.client().delete('movies/1')
+        res = self.client().delete('movies/1',
+                                   headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -158,7 +209,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['movies']), 0)
 
     def test_3_delete_actor(self):
-        res = self.client().delete('actors/1')
+        res = self.client().delete('actors/1',
+                                   headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -166,7 +218,8 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(len(data['actors']), 0)
 
     def test_delete_non_existing_actor_yield_404(self):
-        res = self.client().delete('actors/5')
+        res = self.client().delete('actors/5',
+                                   headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -174,14 +227,70 @@ class CastingTestCase(unittest.TestCase):
         self.assertEqual(data['message'], "Resource was not found")
 
     def test_delete_non_existing_movie_yield_404(self):
-        res = self.client().delete('movies/5')
+        res = self.client().delete('movies/5',
+                                   headers=self.auth_header_executive)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "Resource was not found")
 
+    # RBAC TESTS
+    # A casting assistant should view actors but not create new movies
+
+    def test_2_get_actors_assistant(self):
+        res = self.client().get('actors',
+                                headers=self.auth_header_assistant)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['actors']), 1)
+
+    def test_post_actors_not_allowed_assistant(self):
+        res = self.client().post('actors', json=self.new_invalid_actor,
+                                 headers=self.auth_header_assistant)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['description'], "Permission not found.")
+        self.assertEqual(data['code'], "unauthorized")
+
+    # A casting director can add and delete actor but not delete or add movies
+
+    def test_4_post_actors_director(self):
+        res = self.client().post('actors', json=self.new_actor,
+                                 headers=self.auth_header_director)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['actors']), 1)
+
+    def test_5_delete_actor_director(self):
+        res = self.client().delete('actors/2',
+                                   headers=self.auth_header_director)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['actors']), 0)
+
+    def test_5_post_movies_director_yield_403(self):
+        res = self.client().post('movies', json=self.new_movie,
+                                 headers=self.auth_header_director)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['description'], "Permission not found.")
+        self.assertEqual(data['code'], "unauthorized")
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
+    AUTH_HEADER_EXECUTIVE = getUserTokenHeaders(
+        'messing.maximilian+executive@gmail.com')
+    AUTH_HEADER_DIRECTOR = getUserTokenHeaders(
+        'messing.maximilian+director@gmail.com')
+    AUTH_HEADER_ASSISTANT = getUserTokenHeaders(
+        'messing.maximilian+assistant@gmail.com')
     unittest.main()
